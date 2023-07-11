9b9a5052a4d0352002ace94d0980503f1e2529cd
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-10 21:54:44 -0400

    beginning to work with form info

--- code.ts
+++ code.ts
@@ -2,3 +2,16 @@ figma.showUI(__html__);
 
 figma.ui.resize(500,500);
 
+figma.ui.onmessage = pluginMessage => {
+    console.log("Checking if VSCode is watching...");
+
+    if (pluginMessage.darkModeState === true) {
+        // thing
+    }
+    else {
+        // other
+    }
+
+    figma.closePlugin();
+}
+
